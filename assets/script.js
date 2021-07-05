7425a7fe46c4723727b63a6778409d7aeac20f8d
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-07-04 20:34:40 -0400

    populated day of information from the API

--- assets/script.js
+++ assets/script.js
@@ -27,8 +27,30 @@ formBtn.addEventListener('click', function() {
         return response.json()
     })
     .then (function(data) {
-        // var unorderList_1 = document.createElement("LI");
-       
+        var unorderList_currentCity = document.createElement("LI");
+        var unorderList_currentTem = document.createElement("LI");
+        var unorderList_currentWind = document.createElement("LI");
+        var unorderList_currentHumidity = document.createElement("LI");
+        var currentCity= (data.name);
+        var currentTemp =(data.main.temp);
+        var currentWindSpeed= (data.wind.speed)
+        var currentHumidityPercent= (data.main.humidity)
+        // City Stuff
+        document.getElementById("topList").appendChild(unorderList_currentCity);
+        unorderList_currentCity.innerHTML = currentCity + moment().format(" MMM Do YY");
+        // Temp Stuff
+        document.getElementById("topList").appendChild(unorderList_currentTem);
+        unorderList_currentTem.innerHTML = currentTemp + " change to F";
+
+        // Wind Stuff
+        document.getElementById("topList").appendChild(unorderList_currentWind);
+        unorderList_currentWind.innerHTML = currentWindSpeed + " mph";
+
+        // Humidity
+        document.getElementById("topList").appendChild(unorderList_currentHumidity);
+        unorderList_currentHumidity.innerHTML = currentHumidityPercent + " %";
+
+        // unorderList_1.appendChild(data.weather[0].description);
         console.log(apiCitySearch)
         console.log(data)
         console.log(data.name)
@@ -38,9 +60,6 @@ formBtn.addEventListener('click', function() {
         // (X − 273.15) × 9/5 + 32 (Kelvin to F equation)
         // console.log(data.weather[0].description)
         // data.weather[0].description
-
-        // unorderList_1.appendChild(data.weather[0].description);
-        // document.getElementById("topList").appendChild(unorderList_1);
     })
 })
 
